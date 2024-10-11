abb651b268397f93a748f95d6653ae30abff8266
selintas <sselintas@gmail.com>
2024-10-11 21:30:31 +0300

    Live Class - Döngüler | 08.10.2024 Hafta 2A Salı gunu sorular - commit 2

---
+++ Week2/HA2aSorular/ForSonsuz/ForSonsuz/Program.cs
@@ -0,0 +1,8 @@
+﻿// saat sonsuz dongu 
+for (; ; ) //while(true) veya while(1 < 2) sonsuz dongu yapar.
+
+{
+    Console.Clear(); // her degısımde temızler alt alta devam etmez.
+    Console.WriteLine(DateTime.Now.ToLongTimeString());
+    Thread.Sleep(1000);
+}

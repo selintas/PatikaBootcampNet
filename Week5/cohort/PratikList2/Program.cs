d1c0392adda37d6d69d8f201a0c18282a842c2a2
selintas <sselintas@gmail.com>
2024-11-07 13:46:16 +0300

    Week5PratikList2

---
+++ Week5/cohort/PratikList2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // List tanımlama
+        List<string> kahveler = new List<string>();
+
+        // Program tanıtımı
+        Console.WriteLine("Bir kahve içsek bize iyi gelecek!");
+
+        // Kullanıcıdan kahve isimlerini al ve listeye ekle
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Kahve {i}: ");
+            string kahve = Console.ReadLine();
+            kahveler.Add(kahve);
+        }
+
+        // Kahve listesini ekrana yazdır
+        Console.WriteLine("\n----- KAHVE LİSTESİ -----");
+        for (int i = 0; i < kahveler.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {kahveler[i]}");
+        }
+
+        //int numara = 1;
+        //foreach (string kahve in kahveler)
+        //{
+        //    Console.WriteLine($"{numara}. {kahve}");
+        //    numara++;
+        //}
+    }
+}

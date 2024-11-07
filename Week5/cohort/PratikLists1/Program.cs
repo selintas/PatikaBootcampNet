d94122de692c0676f0010e43d191d9a5150c77a5
selintas <sselintas@gmail.com>
2024-11-07 13:21:14 +0300

    Week5PRatikList1-update

--- Week5/cohort/PratikLists1/Program.cs
+++ Week5/cohort/PratikLists1/Program.cs
@@ -31,9 +31,41 @@ class Program
         // Davetli listesini yazdır
         Console.WriteLine("\n------- DAVETLİLER LİSTESİ -------");
 
+        int numara = 1;
         foreach (string isim in davetliler)
         {
-            Console.WriteLine(isim);
+            Console.WriteLine($"{numara}. {isim}");
+            numara++;
         }
     }
 }
+
+//﻿using System;
+//using System.Collections.Generic;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // Ziyaretçilerin isimlerini içeren bir liste tanımlayın
+//        List<string> ziyaretciler = new List<string>()
+//        {
+//            "1.Bülent Ersoy",
+//            "2.Ajda Pekkan",
+//            "3.Ebru Gündeş",
+//            "4.Hadise",
+//            "5.Hande Yener",
+//            "6.Tarkan",
+//            "7.Funda Arar",
+//            "8.Demet Akalın"
+//        };
+
+//        // İsimleri tek tek ekrana yazdır
+//        Console.WriteLine("Ziyaretçi Listesi:");
+
+//        foreach (string isim in ziyaretciler)
+//        {
+//            Console.WriteLine(isim);
+//        }
+//    }
+//}

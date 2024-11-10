2d202cfab23fbad6c14028e2a6b4c36aea013d93
selintas <sselintas@gmail.com>
2024-11-10 21:19:25 +0300

    Week6SQLkomurları1234
    odev2

--- Week6/odev1234/odev1.sql
+++ Week6/odev1234/odev1.sql
@@ -10,7 +10,7 @@
 
 --film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
 --SELECT * fROM film 
---WHERE (rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99); 
+--WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99); 
 
 --customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 --SELECT last_name FROM customer

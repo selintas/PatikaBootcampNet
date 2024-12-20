fd77676c59c3d0045a4d8ace4af4ba7e935f74b0
selintas <sselintas@gmail.com>
2024-12-20 22:15:05 +0300

    Patch update  - CrazyMusicians week11

--- Week11/CrazyMusiciansAPI/CrazyMusiciansAPI/Controllers/MusiciansController.cs
+++ Week11/CrazyMusiciansAPI/CrazyMusiciansAPI/Controllers/MusiciansController.cs
@@ -1,8 +1,10 @@
 ﻿using CrazyMusiciansAPI.Data;
 using CrazyMusiciansAPI.Dtos;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics.Metrics;
 using System.Xml.Linq;
+//using Microsoft.AspNetCore.JsonPatch;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,7 +22,7 @@ namespace CrazyMusiciansAPI.Controllers
         }
 
         // GET api/<MusicianController>/5
-        [HttpGet("{id}")]
+        [HttpGet("{id:int:min(1)}", Name = "GetMusicianbyId")]
         public ActionResult<Musician> Get(int id)
         {
             Musician? musician = DataStore.Musicians().FirstOrDefault(x => x.Id == id);
@@ -29,9 +31,24 @@ namespace CrazyMusiciansAPI.Controllers
                 return NotFound();
 
             return musician;
-        }
 
-        // POST api/<MusiciansController>
+        }
+        ////ornek uygulama
+        [HttpGet("search/{keyword:alpha:minlength(3)}")]
+        ////search?keyword=deneme& page=1
+        public IActionResult Search(string keyword)
+        {
+            return Ok();
+        }
+        ////ornek uygulama
+        //[HttpGet("search")]
+        ////search?keyword=deneme& page=1
+        //public IActionResult Search([FromQuery] string keyword, [FromQuery] int? page = 1)
+        //{
+        //    return Ok();
+        //}
+        
+        // POST api/<MusiciansController>  //POST eklemek
         [HttpPost]
         public ActionResult<Musician> Post(PostMusicianDto dto)
         {
@@ -47,10 +64,10 @@ namespace CrazyMusiciansAPI.Controllers
             // 201 creadted
             return CreatedAtAction("Get", new {id =musician.Id}, musician);
         }
-
+        //PUT(Create - Oluştur)
         // PUT api/<MusiciansController>/5
         [HttpPut("{id}")]
-        public IActionResult Put(int id, PutMusicianDto dto)
+        public IActionResult Put(int id, PutMusicianDto dto) //(Create - Oluştur)
         {
             if (id != dto.Id)
                 return BadRequest(); 
@@ -64,9 +81,38 @@ namespace CrazyMusiciansAPI.Controllers
             musician.Genre = dto.Genre;
             musician.YearsActive = dto.YearsActive;
 
-            return NotFound(); //204 no content içerik yok
+            return NoContent(); // Başarılı güncelleme, içerik yok (204) //204 no content içerik yok
 
         }
+        // Patch 
+        // PATCH api/<MusiciansController>/5
+        // PATCH: api/Musician/{id}
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromQuery] string? name, [FromQuery] string? instrument, [FromQuery] string? genre, [FromQuery] int? YearsActive)
+        {
+            // İlgili müzisyeni bul
+            Musician? musician = DataStore.Musicians().FirstOrDefault(m => m.Id == id);
+            if (musician == null)
+                return NotFound("Musician not found."); // 404: Müzisyen bulunamadı
+
+            // Gönderilen parametrelere göre alanları güncelle
+            if (!string.IsNullOrEmpty(name))
+                musician.Name = name;
+
+            if (!string.IsNullOrEmpty(instrument))
+                musician.Instrument = instrument;
+
+            if (!string.IsNullOrEmpty(genre))
+                musician.Genre = genre;
+
+            // yearsActive kontrolü ve güncellemesi
+            // yearsActive kontrolü ve güncellemesi
+            if (YearsActive != null && YearsActive > 0)
+                musician.YearsActive = YearsActive.Value;
+
+            return Ok(musician); // Güncellenmiş müzisyeni döndür
+        }
+
 
         // DELETE api/<MusiciansController>/5
         [HttpDelete("{id}")]
--- Week11/CrazyMusiciansAPI/CrazyMusiciansAPI/Data/Musician.cs
+++ Week11/CrazyMusiciansAPI/CrazyMusiciansAPI/Data/Musician.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; } = "";
         public string Instrument { get; set; } = "";
         public string Genre { get; set; } = "";
-        public int YearsActive { get; set; }
+        public int? YearsActive { get; set; }
     }
 }

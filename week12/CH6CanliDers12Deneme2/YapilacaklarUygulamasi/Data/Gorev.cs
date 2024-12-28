2b5d37038d87c528b64255e55b0dc981f3143ee8
selintas <sselintas@gmail.com>
2024-12-29 00:56:13 +0300

    CH6CanliDers12_YapilacaklarUygulamasi

---
+++ week12/CH6CanliDers12Deneme2/YapilacaklarUygulamasi/Data/Gorev.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YapilacaklarUygulamasi.Data
+{
+    public class Gorev
+    {
+        public int Id { get; set; }
+
+        //[MaxLength(400)]
+        public string Baslik { get; set; } = "";
+
+        public bool Yapildi { get; set; }
+    }
+}

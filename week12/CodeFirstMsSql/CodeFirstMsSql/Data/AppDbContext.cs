edd8f951f927ef911c71296d56076a20da6c57c7
selintas <sselintas@gmail.com>
2024-12-30 17:05:26 +0300

    week12-Beraber Code First Yaklaşım

---
+++ week12/CodeFirstMsSql/CodeFirstMsSql/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstMsSql.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<StudentEntity> Students { get; set; } // StudentEntity yapılarının tutulacağı veritablonuzun adını Students olarak belirlediniz.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // @ -> ters slash sorun yaratmasın diye.
+
+            // Windows Authentication için
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=1234;Database=CodeFirstDB");
+
+            // Sql Server Authentication için
+            // optionsBuilder.UseSqlServer(@"server=SrtnWorkBook-2; database=PatikaFirstDB; uid=sa; pwd=sa; TrustServerCertificate=true");
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ week12/CodeFirstMsSql/CodeFirstMsSql/Data/StudentEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstMsSql.Data
+{
+    public class StudentEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(400)]
+        public string FirstName { get; set; } = "";
+
+        [MaxLength(400)]
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+    } 
+
+}

1887462f4aee52acbfc7d369546bc2f114ae5619
selintas <sselintas@gmail.com>
2024-12-30 19:11:40 +0300

    Entities

---
+++ week12/PratikCodeFirstBasic/PratikCodeFirstBasic/Data/Entities/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace PratikCodeFirstBasic.Data.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; } // Birincil Anahtar
+        public string Title { get; set; } // Filmin Başlığı
+        public string Genre { get; set; } // Filmin Türü
+        public int ReleaseYear { get; set; } // Filmin Çıkış Yılı
+    }
+}

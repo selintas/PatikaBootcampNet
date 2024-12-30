ee611d4dc7fda4ea5c1d0438e886373f641818bc
selintas <sselintas@gmail.com>
2024-12-30 23:10:22 +0300

    Pratik - Code First Basic -week12

---
+++ week12/PratikCodeFirstBasic/PratikCodeFirstBasic/Data/PatikaFirstDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace PratikCodeFirstBasic.Data
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; } // tablolar oluştur.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=1234;Database=PatikaCodeFirstDb1");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movies");
+            modelBuilder.Entity<Game>().ToTable("Games");
+
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie() { Id = 1, Title = "Tatile çık", Genre = "Aksiyon", ReleaseYear = 2012 },
+                new Movie() { Id = 2, Title = "Evlen", Genre = "Comedy", ReleaseYear = 2024 },
+                new Movie() { Id = 3, Title = "Saygınlık kazan", Genre = "Drama", ReleaseYear =2014 }
+                );
+
+            modelBuilder.Entity<Game>().HasData(
+                new Game() { Id = 1, Name = "Tatile çık", Platform = "PC", Rating = 10 },
+                new Game() { Id = 2, Name = "Evlen", Platform = "PlayStation", Rating = 1 },
+                new Game() { Id = 3, Name = "Saygınlık kazan", Platform = "Xbox", Rating = 7}
+                );
+        }
+
+    }
+}
---
+++ week12/PratikCodeFirstBasic/PratikCodeFirstBasic/Migrations/20241230200257_First.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PratikCodeFirstBasic.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Platform = table.Column<string>(type: "text", nullable: false),
+                    Rating = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Genre = table.Column<string>(type: "text", nullable: false),
+                    ReleaseYear = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Games",
+                columns: new[] { "Id", "Name", "Platform", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Tatile çık", "PC", 10m },
+                    { 2, "Evlen", "PlayStation", 1m },
+                    { 3, "Saygınlık kazan", "Xbox", 7m }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Genre", "ReleaseYear", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Aksiyon", 2012, "Tatile çık" },
+                    { 2, "Comedy", 2024, "Evlen" },
+                    { 3, "Drama", 2014, "Saygınlık kazan" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}

3c5dc7038e34fe89b2bba9f80e07d362a795b8e8
selintas <sselintas@gmail.com>
2025-01-04 22:42:51 +0300

    Entities-EntityFrameworks

---
+++ week12/CodeFirstAraTablo/CodeFirstAraTablo/Data/Entities/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeFirstAraTablo.Data.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+
+        // Dersin alındığı öğrencilerin listesi
+        public List<StudentCourse>? StudentCourses { get; set; }
+    }
+}
---
+++ week12/CodeFirstAraTablo/CodeFirstAraTablo/Data/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeFirstAraTablo.Data.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+
+        // Öğrencinin aldığı derslerin listesi
+        public List<StudentCourse>? StudentCourses { get; set; } 
+    }
+}

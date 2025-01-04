35efd77c0eae47d40e719e0bca6627fea70455a5
selintas <sselintas@gmail.com>
2025-01-04 22:18:49 +0300

    HA12BLİveClas- DoyaDoyaAnadolu

---
+++ week12/CH6CanliDers12Deneme2/DoyaDoyaAnadolu/Data/Sehir.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoyaDoyaAnadolu.Data
+{
+    public class Sehir
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string SehirAd { get; set; } = "";
+
+        public int Nufus { get; set; }
+
+        public int BolgeId { get; set; }
+
+
+        public Bolge Bolge { get; set; } = null!;
+    }
+}
